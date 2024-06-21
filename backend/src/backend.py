8bbdac6db601b125ee404a344fde2dd377eab1ec
daspien <daspien4@gmail.com>
2024-06-21 23:46:48 +0200

    Finished backend

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+
+from bson import ObjectId
 
 backend = Flask(__name__)
 backend.config['MONGO_URI']='mongodb://localhost/dataSeekers'
@@ -8,23 +10,67 @@ mongo = PyMongo(backend)
 
 CORS(backend)
 
-db = mongo.db.dataSeekers
+db = mongo.db.users
 
 @backend.route('/users', methods=['POST'])
 def createUser():
-    db.insert_one({
-        'name': 'juan'
+    id = db.insert_one({
+        'name': request.json['name'],
+        'connections': []
     })
-    return 'received'
+    return jsonify(str(id.inserted_id))
 
 @backend.route('/users', methods=['GET'])
 def getUsers():
-    print(request.json)
-    return 'received'
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name': doc['name'],
+            'connections': str(doc['connections'])
+        })
+    return jsonify(users)
+
+@backend.route('/users/<id>/<id2>', methods=['PUT'])
+def connectUsers(id, id2):
+    user1 = db.find_one({'_id': ObjectId(id)})
+    user2 = db.find_one({'_id': ObjectId(id2)})
+    connections1 = user1['connections']
+    connections2 = user2['connections']
+    connections1.append(user2['_id']) if user2['_id'] not in connections1 else connections1
+    connections2.append(user1['_id']) if user1['_id'] not in connections2 else connections2
+    db.update_one({'_id': ObjectId(id)}, {"$set": {
+        'name': user1['name'],
+        'connections': connections1
+    }})
+    db.update_one({'_id': ObjectId(id2)}, {"$set": {
+        'name': user2['name'],
+        'connections': connections2
+    }})
+    return jsonify({'message': 'Users connected'})
 
 @backend.route('/user/<id>', methods=['GET'])
-def getUser():
-    return 'received'
+def getUser(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    return jsonify({
+      '_id': str(ObjectId(user['_id'])),
+      'name': user['name'],
+      'connections': str(user['connections'])
+  })
+
+@backend.route('/userConnections/<id>', methods=['GET'])
+def getUserConnections(id):
+    user = db.find_one({'_id': ObjectId(id)})
+    connections = []
+    for doc in user['connections']:
+        connected = db.find_one({'_id': ObjectId(doc)})
+        connections.append({
+            '_id': str(connected['_id']),
+            'name': connected['name'],
+            'connections': str(connected['connections'])
+        })
+    return jsonify(connections)
+
 
 if __name__ == "__main__":
     backend.run(debug=True)
